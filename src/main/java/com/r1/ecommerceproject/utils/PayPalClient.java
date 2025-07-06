6adbb4b55291fc0e97185203833d375fea1f3f8c
alexfg04 <alessioforgione42@gmail.com>
2025-07-06 18:00:25 +0200

    Tolto le API KEYS di PayPal per ragioni di sicurezza

--- src/main/java/com/r1/ecommerceproject/utils/PayPalClient.java
+++ src/main/java/com/r1/ecommerceproject/utils/PayPalClient.java
@@ -4,8 +4,8 @@ import com.paypal.core.PayPalEnvironment;
 import com.paypal.core.PayPalHttpClient;
 
 public class PayPalClient {
-    private static final String CLIENT_ID = "AcO8UtjKGoOmhnB6AMJvH4fHC8N337YndFwEVaYWubBss4gduwBGE-9xpgUg8DUMJpn6b1E3pd8XgPTQ";
-    private static final String CLIENT_SECRET = "EM3f3vzx6gvu2zFLwtM2nMjRlFFT11k8vUZ6z-1kvAXlH1k5j1HvysJ5R4NPeV78_wydm3uqGcq7Q5-a";
+    private static final String CLIENT_ID = System.getenv("PAYPAL_CLIENT_ID");
+    private static final String CLIENT_SECRET = System.getenv("PAYPAL_CLIENT_SECRET");
 
     // Ambiente sandbox
     private static final PayPalEnvironment environment =
