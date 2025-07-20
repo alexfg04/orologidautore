142c28b380f0d8dce9c43e71a03d38700896f982
alexfg04 <alessioforgione42@gmail.com>
2025-07-20 15:08:05 +0200

    Fix Servlet SalvaModificheProdottoServlet.java

--- src/main/java/com/r1/ecommerceproject/servlet/admin/SalvaModificheProdottoServlet.java
+++ src/main/java/com/r1/ecommerceproject/servlet/admin/SalvaModificheProdottoServlet.java
@@ -35,27 +35,7 @@ public class SalvaModificheProdottoServlet extends HttpServlet {
         String taglia = request.getParameter("taglia");
         String materiale = request.getParameter("materiale");
         String iv = request.getParameter("iva");
-        BigDecimal iva = BigDecimal.valueOf(Double.parseDouble(iv));
-
-        response.setContentType("text/html;charset=UTF-8");
-
-        PrintWriter out = response.getWriter();
-        out.println("<html><head><title>Test Parametri Form</title></head><body>");
-        out.println("<h2>Parametri ricevuti:</h2>");
-        out.println("<ul>");
-        out.println("<li>Nome: " + id + "</li>");
-        out.println("<li>Nome: " + nome + "</li>");
-        out.println("<li>Marca: " + marca + "</li>");
-        out.println("<li>Categoria: " + categoria + "</li>");
-        out.println("<li>Prezzo: " + prezzo + "</li>");
-        out.println("<li>Modello: " + modello + "</li>");
-        out.println("<li>Descrizione: " + descrizione + "</li>");
-        out.println("<li>Taglia: " + taglia + "</li>");
-        out.println("<li>Materiale: " + materiale + "</li>");
-        out.println("<li>Iva: " + iva + "</li>");
-        out.println("</ul>");
-        out.println("</body></html>");
-
+        BigDecimal iva = new BigDecimal(iv);
 
         // Popola il bean
         ProductBean prodotto = new ProductBean();
@@ -63,38 +43,20 @@ public class SalvaModificheProdottoServlet extends HttpServlet {
         prodotto.setNome(nome);
         prodotto.setMarca(marca);
         prodotto.setGenere(categoria);
+        prodotto.setIvaPercentuale(iva);
         prodotto.setPrezzo(prezzo);
         prodotto.setModello(modello);
         prodotto.setDescrizione(descrizione);
         prodotto.setTaglia(taglia);
         prodotto.setMateriale(materiale);
 
-        // Stampa a schermo
-        PrintWriter ou = response.getWriter();
-        ou.println("<html><head><title>Bean Ricevuto</title></head><body>");
-        ou.println("<h1>Valori del ProductBean:</h1>");
-        ou.println("<ul>");
-        ou.println("<li><strong>Id:</strong> " + prodotto.getCodiceProdotto() + "</li>");
-        ou.println("<li><strong>Nome:</strong> " + prodotto.getNome() + "</li>");
-        ou.println("<li><strong>Marca:</strong> " + prodotto.getMarca() + "</li>");
-        ou.println("<li><strong>Genere (Categoria):</strong> " + prodotto.getGenere() + "</li>");
-        ou.println("<li><strong>Prezzo:</strong> " + prodotto.getPrezzo() + "</li>");
-        ou.println("<li><strong>Modello:</strong> " + prodotto.getModello() + "</li>");
-        ou.println("<li><strong>Descrizione:</strong> " + prodotto.getDescrizione() + "</li>");
-        ou.println("<li><strong>Taglia:</strong> " + prodotto.getTaglia() + "</li>");
-        ou.println("<li><strong>Materiale:</strong> " + prodotto.getMateriale() + "</li>");
-        ou.println("</ul>");
-
         // DAO call
         ProductDao productDao = new ProductDaoImpl();
         try {
             productDao.updateProduct(prodotto);
-            out.println("<p style='color: green;'>Prodotto aggiornato correttamente nel database.</p>");
+            response.sendRedirect(request.getContextPath() + "/admin/dashboard.jsp#tableProdotti");
         } catch (Exception e) {
-            out.println("<p style='color: red;'>Errore durante l'aggiornamento: " + e.getMessage() + "</p>");
+            throw new ServletException(e);
         }
-
-        out.println("</body></html>");
-
     }
 }
